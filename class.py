be9e9344ec85e25fa61a1415bdaec9baa3d69e91
Siddhant Dubey <siddhantdubey.k07@gmail.com>
2026-03-22 09:08:32 +0530

    Update class.py

--- class.py
+++ class.py
@@ -1,29 +1,29 @@
-# count = int(input("Enter a number : "))
-# while count>0 and count<=100:
-#   print(count)
-#   count=count-1
+count = int(input("Enter a number : "))
+while count>0 and count<=100:
+  print(count)
+  count=count-1
 
-# password=""
-# while password != "Python@123":
-#   password = input("Enter Password : ")
-#   if password != "Python@123":
-#     print("Wrong Password")
-#   else: print("Access is granted!!") 
+password=""
+while password != "Python@123":
+  password = input("Enter Password : ")
+  if password != "Python@123":
+    print("Wrong Password")
+  else: print("Access is granted!!") 
 
-# number=int(input("Enter a number : "))
-# while number != 1:
-#     if number % 2 == 0:
-#        number = number/2
-#     else: number = number*3+1
-# print (number)
+number=int(input("Enter a number : "))
+while number != 1:
+    if number % 2 == 0:
+       number = number/2
+    else: number = number*3+1
+print (number)
 
-# def Mean(data):
-#     total = sum(data)
-#     n = len (data)
-#     mean = total/n
-#     return print (mean)
-# A=[10,20,30,40,50]
-# Mean(A)
+def Mean(data):
+    total = sum(data)
+    n = len (data)
+    mean = total/n
+    return print (mean)
+A=[10,20,30,40,50]
+Mean(A)
 
 a = [1, 2, 3]
 b = [1, 2, 3]
